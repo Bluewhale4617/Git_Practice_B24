34a1d95f7ab98a7a73ab55601ea4166db6c4cbe3
BetulYalcin <bbakiciyalcin@gmail.com>
2021-10-03 16:32:44 -0400

    new file

---
+++ src/Test/WoodenSpoonTest.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class WoodenSpoonTest {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++){
+            System.out.println(" Wooden Spoon");
+            System.out.println("Betul");
+        }
+    }
+}

4e7dd03aa7ee665e891e526526f24b4db2b7b7e6
Betul Yalcin <bbakiciyalcin@gmail.com>
2021-10-03 11:34:02 -0400

    setup new project added gitignore first.. thirde commit

--- src/setup/first.java
+++ src/setup/first.java
@@ -3,5 +3,10 @@ package setup;
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //This is a new line
+        //I want to create a history
+        //The text does not matter
+        // third commit
+        //more lines
     }
 }

a01c8c6a5919ed8e1c7f9e14509231b12758f21c
Betul Yalcin <bbakiciyalcin@gmail.com>
2021-10-03 16:51:57 -0400

    updated HQQ

--- src/tests2/HelloWorldTest2.java
+++ src/tests2/HelloWorldTest2.java
@@ -3,5 +3,6 @@ package tests2;
 public class HelloWorldTest2 {
     public static void main(String[] args) {
         System.out.println("HelloWorld!!!!");
+        System.out.println("HelloWorld!!!!!!");
     }
 }

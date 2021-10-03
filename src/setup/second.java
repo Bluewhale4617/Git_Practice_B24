f2128cc8a24af72e28bbe354f2f2968c6e62bbaf
Betul Yalcin <bbakiciyalcin@gmail.com>
2021-10-03 12:48:24 -0400

    setup new project second and third classes are done. sout comments were added

--- src/setup/second.java
+++ src/setup/second.java
@@ -3,5 +3,6 @@ package setup;
 public class second {
     public static void main(String[] args) {
         System.out.println("Hello Universe!");
+        System.out.println("Hello");
     }
 }
--- src/setup/third.java
+++ src/setup/third.java
@@ -1,5 +1,8 @@
 package setup;
 
 public class third {
-
+    public static void main(String[] args) {
+        System.out.println("Hello Universe!");
+        System.out.println("Hello");
+    }
 }

f9229869ae0650b0205511eaf61f1819137c049d
Betul Yalcin <bbakiciyalcin@gmail.com>
2021-10-03 16:52:24 -0400

    updated Hellow world One more

--- src/tests2/HelloWorldTest2.java
+++ src/tests2/HelloWorldTest2.java
@@ -4,5 +4,6 @@ public class HelloWorldTest2 {
     public static void main(String[] args) {
         System.out.println("HelloWorld!!!!");
         System.out.println("HelloWorld!!!!!!");
+        System.out.println("HelloWorld!!!!!!");
     }
 }

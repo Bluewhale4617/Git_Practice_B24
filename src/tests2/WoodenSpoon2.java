ae2804be95b93e979812547d465df6f45b475ee3
Betul Yalcin <bbakiciyalcin@gmail.com>
2021-10-03 16:43:46 -0400

    woodenspoon print2

--- src/tests2/WoodenSpoon2.java
+++ src/tests2/WoodenSpoon2.java
@@ -2,6 +2,8 @@ package tests2;
 
 public class WoodenSpoon2 {
     public static void main(String[] args) {
+
+        System.out.println("WoodenSpoon");
         System.out.println("WoodenSpoon");
     }
 }
